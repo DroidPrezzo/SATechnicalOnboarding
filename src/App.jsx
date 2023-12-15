6a39cb3b4692b15862946b4dbb6cd53e069a068a
Precious Oluwafemi Sani <33557670+DroidPrezzo@users.noreply.github.com>
2023-12-15 18:08:55 +0000

    edited page with personal details

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,15 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
+  name: "Precious Sani",
+  title: "Data & Analytics Consultant | Data Engineer",
+  email: "poluwafemisani@gmail.com",
+  gitHub: "DroidPrezzo",
+  instagram: "",
+  linkedIn: "pfemisani",
   medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  twitter: "pfemis",
+  youTube: "@CodewithPrecious",
 };
 
 const primaryColor = "#4E567E";
